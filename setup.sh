a6392538b485438ccfab22b680f03980a988b7e1
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2016-05-24 12:21:45 -0700

    aws cli

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ cd /root/micro-api
 # get the pre installed nodemodules to here to save demo time
 # comment this out to do native install
 mv /root/micro-image/node_modules .
+sudo apt-get install -y python-pip 
 sudo pip install awscli
 # install node modules. the above mv will make this zip through as nothing new is installed
 npm install
